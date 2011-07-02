ff0844fdbfcde6c23585b97805cb2d4b843b0af9
V Sreekanth <sreeix@gmail.com>
2011-07-03 02:12:23 +0530

    Adding redis storage for storing history of the statuses

---
+++ plugins/redis_storage.js
@@ -0,0 +1,32 @@
+// Needs to export a create method that takes a handle to the controller.
+// up/down/unknown/critical are expected messages. Probably need to add transition status up -> down, down->up or up->critical
+// Needs redis to be running.
+// needs noderedis. install it via npm install hiredis redis
+var redis = require("redis"),client = redis.createClient();
+
+var storeStatus = function (service){
+  client.rpush("statusdashboard:"+service.name, JSON.stringify({time: new Date().valueOf(), status: service.status, message: service.message, code:service.statusCode}));
+  
+}    
+exports.create = function(api){
+  console.log('Creating the Redis storage plugin');
+  api.on('up', function(service){
+    storeStatus(service);
+    console.log('success...'+ service.name);
+  });  
+
+  api.on('down', function(service){
+    storeStatus(service);
+    console.log('down...'+ service.name);
+  });  
+
+  api.on('unknown', function(service){
+    storeStatus(service);
+    console.log('unknown...'+ service.name);
+  });  
+
+  api.on('critical', function(service){
+    storeStatus(service);
+    console.log('critical...'+ service.name);
+  });  
+};
