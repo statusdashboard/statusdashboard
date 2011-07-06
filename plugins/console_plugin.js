7098d44314aa21da8c6ac46bd9e486a151897b4c
obazoud <olivier.bazoud@gmail.com>
2011-07-06 13:42:30 +0200

    Console plugin: add enable flag

--- plugins/console_plugin.js
+++ plugins/console_plugin.js
@@ -1,26 +1,28 @@
 // Needs to export a create method that takes a handle to the controller.
 // up/down/unknown/critical are expected messages. Probably need to add transition status up -> down, down->up or up->critical
 
-exports.create = function(api) {
-  console.log('Creating the plugin: ' + __filename);
-  // new route should be: /api/[pluginName]/whatever
-  api.emit("routeContribution", { method: 'GET', path: /^\/api\/console\/test$/, binding: module.exports.myRouteContribution });
+exports.create = function(api, settings) {
+  if (settings.plugins && settings.plugins.console && settings.plugins.console.enable) {
+    console.log('Creating the plugin: ' + __filename);
+    // new route should be: /api/[pluginName]/whatever
+    api.emit("routeContribution", { method: 'GET', path: /^\/api\/console\/test$/, binding: module.exports.myRouteContribution });
 
-  api.on('up', function(service){
-    console.log('success...' + service.name);
-  });  
+    api.on('up', function(service){
+      console.log('success...' + service.name);
+    });
 
-  api.on('down', function(service){
-    console.log('failed...' + service.name);
-  });  
+    api.on('down', function(service){
+      console.log('failed...' + service.name);
+    });
 
-  api.on('unknown', function(service){
-    console.log('unknown...' + service.name);
-  });  
+    api.on('unknown', function(service){
+      console.log('unknown...' + service.name);
+    });
 
-  api.on('critical', function(service){
-    console.log('critical...' + service.name);
-  });
+    api.on('critical', function(service){
+      console.log('critical...' + service.name);
+    });
+  }
 };
 
 module.exports.myRouteContribution = function(req, res) {
