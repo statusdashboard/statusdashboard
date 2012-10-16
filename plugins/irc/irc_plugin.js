ae6411ba68c21e04ed2fec9b6240ae108873b622
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-10-16 23:14:39 +0200

    Get status by sending private message to IRC bot

--- plugins/irc/irc_plugin.js
+++ plugins/irc/irc_plugin.js
@@ -70,6 +70,17 @@ exports.create = function(api, settings) {
         cache = [];
       }
     });
+    
+    // Add a private message listener to interact with the IRC bot
+    bot.addListener('pm', function(nick, message) {
+        console.log('Got private message from %s: %s', nick, message);
+        var callme = commands[trim(message)];
+        if (typeof callme !== 'undefined') {
+          bot.say(nick, callme.call(null, api));
+        } else {
+          bot.say(nick, commands['help'].call(null, api));
+        }
+    });
   }
   
   // Send the message or cache it
@@ -82,5 +93,25 @@ exports.create = function(api, settings) {
         cache.push(message);
       }
     }
+  }
+  
+  var commands = {
+    status : function(api) {
+      return 'Up: ' + api.getStatus().summarize.up + ', ' + 'Critical: ' + api.getStatus().summarize.critical + ', Down: ' + api.getStatus().summarize.down + ', Unknown: ' + api.getStatus().summarize.unknown;
+    },
+    list : function(api) {
+      var result = '';
+      _.each(api.getStatus().services, function(service){
+        result += "[" + service.status.toUpperCase() + "] " + service.name + ", Code: " +  service.statusCode + ", Message: " + service.message + "\n";
+      });
+      return result;
+    },
+    help: function(api) {
+      return "Available commands : \'list\', \'status\', \'help\'";
+    }
+  };
+  
+  var trim = function(string) {
+    return string.replace(/^\s+/g,'').replace(/\s+$/g,'');
   }
 };
