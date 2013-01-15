4cad66f94c7b5cf6a9041286a5429b7fc07c7c2e
Marc Trudel <mtrudel@wizcorp.jp>
2013-01-15 20:11:29 +0900

    * Refactored api.js, now contains only api calls and the check loop * Refactored server.js so that it can become embeddable as a ndoe module * created bin/statusdashboard, to be used as a binary app instead of server.js * created lib/sources, for creating different source check * created lib/checks, for creating shared checks between sources * All touched code should now pass standard jshint test

--- server.js
+++ server.js
@@ -1,97 +1,85 @@
-var path = require('path');
-var util = require('util');
-var url = require('url');
-var _ = require('underscore')._;
+var path = require('path'),
+    util = require('util'),
+    url = require('url'),
+    _ = require('underscore')._;
 
-process.on('exit', function () {
-  util.log('Bye bye Statusdashboard.');
-});
+exports.dashboard = function(settings) {
+  var rootPath = process.cwd(),
+      api = require('./api'),
+      express = require('express'),
+      app = express.createServer();
 
-process.on('uncaughtException', function(err) {
-  util.log(err);
-});
+  app.configure(function () {
+    var staticPath = path.join(rootPath, 'public');
 
-String.prototype.startsWith = function(str) {
-    return (this.indexOf(str) === 0);
-}
-
-// Directories
-var rootPath = process.cwd();
-var staticPath = path.join(rootPath, 'public');
-
-// Settings
-var settings = require('./settings').create();
-var api = require('./api');
-
-// Server
-var express = require('express')
-var app = express.createServer();
-app.configure(function () {
-  util.log("Express server static directory: " + staticPath);
-  app.use(express.static(staticPath));
-  app.use(express.favicon(path.join(staticPath, 'favicon.ico')));
-  app.use(express.logger());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-});
-var port = process.env.PORT || settings.port;
-app.listen(port, settings.hostname);
-util.log('Server running at http://' + settings.hostname + ':' + port);
-
-// Routes
-app.get(/^\/api\/services$/, api.services);
-app.get(/^\/api\/services\/([a-z\-]+)$/, api.servicesElement);
-app.get(/^\/api\/summarize$/, api.summarize);
-app.get(/^\/api\/config\/client$/, api.configClient);
-app.get(/^\/api\/plugins\/client$/, api.pluginsClient);
-app.get(/^\/api\/uptime$/, api.uptime);
-app.get(/^\/api\/info$/, api.info);
-
-app.get('/healthCheck', function(req, res) {
-  res.send('ok', 200);
-});
-
-api.on("routeContribution", function(route) {
-  app.get(route.path, route.binding);
-  util.log("Add GET route contribution: " + route.path);
-});
-
-api.on("postRouteContribution", function(route) {
-  app.post(route.path, route.binding);
-  util.log("Add POST route contribution: " + route.path);
-});
-
-api.on("staticContribution", function(plugin) {
-  var docRoot = __dirname + '/plugins/' + plugin + '/public';
-  app.use("/api/" + plugin, express.static(docRoot));
-  util.log("Add static contribution: " + plugin + ", " + docRoot);
-});
-
-var count = 0;
-var io = require('socket.io').listen(app);
-// https://github.com/LearnBoost/Socket.IO/wiki/Configuring-Socket.IO
-io.configure(function () {
-  io.enable('browser client minification');
-  io.enable('browser client etag');
-  io.set('log level', 1);
-});
-
-io.sockets.on('connection', function(socket) {
-  count++;
-  util.log('New client connected! (' + count + ')');
-  socket.emit('title', settings.title);
-  socket.emit('status', api.getStatus());
-  io.sockets.emit('count', count);
-
-  socket.on('disconnect', function() {
-    count--;
-    util.log('Client disconnect! (' + count + ')');
-    socket.broadcast.emit('count', count);
+    util.log("Express server static directory: " + staticPath);
+    app.use(express['static'].call(null, staticPath));
+    app.use(express.favicon(path.join(staticPath, 'favicon.ico')));
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
   });
-});
 
-api.on("refresh", function(status) {
-  io.sockets.emit('status', status);
-});
+  app.listen(settings.port, settings.hostname);
 
-util.log('Server started.');
+  util.log('Server running at http://' + settings.hostname + ':' + settings.port);
+
+  // Routes
+  app.get(/^\/api\/services$/, api.services);
+  app.get(/^\/api\/services\/([a-z\-]+)$/, api.servicesElement);
+  app.get(/^\/api\/summarize$/, api.summarize);
+  app.get(/^\/api\/config\/client$/, api.configClient);
+  app.get(/^\/api\/plugins\/client$/, api.pluginsClient);
+  app.get(/^\/api\/uptime$/, api.uptime);
+  app.get(/^\/api\/info$/, api.info);
+
+  app.get('/healthCheck', function(req, res) {
+    res.send('ok', 200);
+  });
+
+  api.on("routeContribution", function(route) {
+    app.get(route.path, route.binding);
+    util.log("Add GET route contribution: " + route.path);
+  });
+
+  api.on("postRouteContribution", function(route) {
+    app.post(route.path, route.binding);
+    util.log("Add POST route contribution: " + route.path);
+  });
+
+  api.on("staticContribution", function(plugin) {
+    var docRoot = __dirname + '/plugins/' + plugin + '/public';
+    app.use("/api/" + plugin, express['static'].call(null, docRoot));
+    util.log("Add static contribution: " + plugin + ", " + docRoot);
+  });
+
+  var count = 0;
+  var io = require('socket.io').listen(app);
+
+  // https://github.com/LearnBoost/Socket.IO/wiki/Configuring-Socket.IO
+  io.configure(function () {
+    io.enable('browser client minification');
+    io.enable('browser client etag');
+    io.set('log level', 1);
+  });
+
+  io.sockets.on('connection', function(socket) {
+    count++;
+    util.log('New client connected! (' + count + ')');
+    socket.emit('title', settings.title);
+    socket.emit('status', api.getStatus());
+    io.sockets.emit('count', count);
+
+    socket.on('disconnect', function() {
+      count--;
+      util.log('Client disconnect! (' + count + ')');
+      socket.broadcast.emit('count', count);
+    });
+  });
+
+  api.on("refresh", function(status) {
+    io.sockets.emit('status', status);
+  });
+
+  util.log('Server started.');
+};
